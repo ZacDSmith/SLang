d6d0c214bda106f45f6d007bc8d6549d7e583e32
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2025-05-21 15:05:41 -0400

    Why * no worky?

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from Lexer import Lexer
+from Parser import Parser
+from AST import Program
+import json
 
-LEXER_DEBUG: bool = True
+LEXER_DEBUG: bool = False
+PARSER_DEBUG: bool = True
 
 if __name__ == '__main__':
-    with open("tests/lexer.sl", "r") as f:
+    with open("tests/parser.sl", "r") as f:
         code: str = f.read()
 
     if LEXER_DEBUG:
         debug_lex: Lexer = Lexer(source=code)
         while debug_lex.current_char is not None:
             print(debug_lex.next_token())
+
+    l: Lexer = Lexer(source=code)
+    p: Parser = Parser(lexer=l)
+
+    if PARSER_DEBUG:
+        print("==== PARSER DEBUG ====")
+        program: Program = p.parse_program()
+
+        with open("debug/ast.json", "w") as f:
+            json.dump(program.json(), f, indent=4)
+
+        print("Wrote AST to debug/ast.json successfully")
